ad31d7617adb894a3c169b12c4a4ad38439306d3
Viktor Kan <html.viktor@gmail.com>
2019-08-15 20:48:24 +0300

    continue

--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,22 @@
-export const makeCard = () => {
+export const makeCard = ({type, time: {start, end}, price}) => {
   return `<li class="trip-events__item">
                   <div class="event">
                     <div class="event__type">
-                      <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+                      <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
                     </div>
-                    <h3 class="event__title">Taxi to airport</h3>
+                    <h3 class="event__title">Title for this</h3>
 
                     <div class="event__schedule">
                       <p class="event__time">
-                        <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+                        <time class="event__start-time" datetime="2019-03-18T10:30">${start}:00</time>
                         —
-                        <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+                        <time class="event__end-time" datetime="2019-03-18T11:00">${end}:00</time>
                       </p>
                       <p class="event__duration">1H 30M</p>
                     </div>
 
                     <p class="event__price">
-                      €&nbsp;<span class="event__price-value">20</span>
+                      €&nbsp;<span class="event__price-value">${price}</span>
                     </p>
 
                     <h4 class="visually-hidden">Offers:</h4>
--- src/data.js
+++ src/data.js
@@ -61,3 +61,4 @@ const makeTasks = (count) => {
 };
 
 export const dataTasks = makeTasks(4);
+console.log(dataTasks)
