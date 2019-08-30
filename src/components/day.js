9b5cd8860000f6e850b7dccdd14dd180f3d798e2
Viktor Kan <html.viktor@gmail.com>
2019-08-30 18:50:46 +0300

    alternative version --good

--- src/components/day.js
+++ src/components/day.js
@@ -1,26 +1,23 @@
 import AbstractComponent from "./abstract-component";
 
 export default class Day extends AbstractComponent {
-  constructor(dates) {
+  constructor(date, index) {
     super();
-    this._dates = dates.sort();
+    this._date = date;
+    this._index = index;
   }
 
   getTemplate() {
     const options = {month: `short`, day: `numeric`};
-    return `<ul class="trip-days">
-    ${this._dates.map((date, index) => {
     return `<li class="trip-days__item  day">
               <div class="day__info">
-                <span class="day__counter">${index + 1}</span>
-                <time class="day__date" datetime="2019-03-18">${new Date(date).toLocaleString(`en-US`, options)}</time>
+                <span class="day__counter">${this._date ? this._index + 1 : ``}</span>
+                <time class="day__date" datetime="2019-03-18">${this._date ? new Date(this._date).toLocaleString(`en-US`, options) : ``}</time>
               </div>
 
               <ul class="trip-events__list">
                 
               </ul>
             </li>`;
-  }).join(``)}
-        </ul>`;
   }
 }
--- src/controllers/trip-controller.js
+++ src/controllers/trip-controller.js
@@ -3,6 +3,7 @@ import Day from "../components/day";
 import {render, Position} from "../utils/utils";
 import PointController from "./point-controller";
 import Sort from "../components/sorting";
+import DaysContainer from "../components/days-container";
 
 export default class TripController extends AbstractComponent {
   constructor(container, data, dates) {
@@ -10,27 +11,50 @@ export default class TripController extends AbstractComponent {
     this._container = container;
     this._data = data;
     this._dates = dates;
-    this._day = new Day(dates);
     this._sort = new Sort();
+    this._daysContainer = new DaysContainer();
   }
 
   init() {
     render(this._container, this._sort.getElement(), Position.BEFORE_END);
-    render(this._container, this._day.getElement(), Position.BEFORE_END);
-    this.renderCards();
-  }
+    render(this._container, this._daysContainer.getElement(), Position.AFTER_END);
+    this.renderDays();
 
-  renderCards() {
-    const daysElement = this._container.querySelectorAll(`.trip-days__item > .trip-events__list`);
-    this._dates.forEach((date, index) => {
-      this._data.filter((it) => it.date === date)
-        .forEach((point) => {
-          const card = new PointController(daysElement[index], point, this.onDateChange.bind(this));
-        });
+    this._sort.getElement().addEventListener(`change`, (evt) => {
+      this.onSort(evt.target.dataset.sort);
     });
   }
 
-  onDateChange(data) {
-
+  renderDays() {
+    this._dates.forEach((date, index) => {
+      const day = new Day(date, index);
+      render(this._daysContainer.getElement(), day.getElement(), Position.AFTER_END);
+      this.renderCards(
+          day.getElement().querySelector(`.trip-events__list`),
+          this._data.filter((it) => it.date === date));
+    });
   }
+
+  renderCards(container, data) {
+    data.forEach((card) => new PointController(container, card));
+  }
+
+  onSort(type) {
+    this._daysContainer.getElement().innerHTML = ``;
+    const day = new Day();
+    render(this._daysContainer.getElement(), day.getElement(), Position.BEFORE_END);
+    switch (type) {
+      case `time`:
+        const sortByTime = [...this._data.sort()];
+        this.renderCards(day.getElement().querySelector(`.trip-events__list`), sortByTime);
+        break;
+      case `price`:
+        const sortByPrice = [...this._data.sort((a, b) => a.price < b.price)];
+        this.renderCards(day.getElement().querySelector(`.trip-events__list`), sortByPrice);
+        break;
+      default:
+        this.renderDays();
+    }
+  }
+
 }
